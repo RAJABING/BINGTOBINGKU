e085ff4798ce38a2f08cd02e73f4f0b46125791c
RAJABING <59326452+RAJABING@users.noreply.github.com>
2020-03-09 17:07:44 +0700

    Add files via upload

---
+++ BINGTOBINGKU.sh
@@ -0,0 +1,39 @@
+clear
+password="BING(BAYU IS KING) "
+until [ "$siapa" = "$password" ]
+do
+  read -p "TULISKAN PASSWORD NYA : " siapa
+done 
+
+
+echo
+toilet -f big -F gay SELAMAT DATANG TUAN
+
+
+echo $green "1.) MELIHAT TOOLS YG TELAH DIBUAT"
+echo $red "2.) MELIHAT PASSWORD YG SUDAH DIBUAT"
+
+read -p "SILAHKAN DI PILIH TUAN : "tuan
+
+
+if [ $tuan = 1 ] || [ $tuan = 1 ]
+then
+clear
+git clone https://github.com/RAJABING/DAFTAR
+ls
+ls DAFTAR
+cd DAFTAR
+ls DAFTAR
+sh DAFTAR.sh
+fi
+
+if [ $tuan = 2 ] || [ $tuan = 2 ]
+then
+clear
+git clone https://github.com/RAJABING/password
+ls
+ls password
+cd password
+ls password
+sh PASSWORD.sh
+fi
